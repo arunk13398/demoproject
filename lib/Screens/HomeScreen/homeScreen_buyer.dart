84e7488116116924d4fa18b521157b006bb443f9
mypc <spinegeneral@gmail.com>
2022-11-27 19:24:07 +0530

    deals page

--- lib/Screens/HomeScreen/homeScreen_buyer.dart
+++ lib/Screens/HomeScreen/homeScreen_buyer.dart
@@ -14,162 +14,170 @@ class _HomeScreenBuyerState extends State<HomeScreenBuyer> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: MyColors.myGrey,
       body: SafeArea(
-        child: Container(
-          color: MyColors.myGrey,
-          padding: const EdgeInsets.symmetric(horizontal: 15),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Row(
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: SingleChildScrollView(
+            child: Container(
+
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
                 children: [
-                  Container(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: const [
-                        Padding(
-                          padding: EdgeInsets.only(top: 20.0),
-                          child: Text(
-                            "Dicker Dealz",
-                            style: TextStyle(
-                                fontSize: 24,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.black),
-                          ),
-                        ),
-                        Text(
-                          "Deal smartly",
-                          style: TextStyle(
-                              fontSize: 16, color: MyColors.myGreyMid3),
-                        ),
-                        SizedBox(
-                          height: 5,
-                        ),
-                      ],
-                    ),
-                  ),
-                  Spacer(),
-                  Container(
-                    height: 50,
-                    width: 50,
-                    decoration: const BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(50)),
-                        color: MyColors.myGreyMid2,
-                        image: DecorationImage(
-                            image: AssetImage("assets/employe.png"))),
-                  )
-                ],
-              ),
-              SingleChildScrollView(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        "Welcome User",
-                        textAlign: TextAlign.start,
-                        style: TextStyle(
-                            fontSize: 30, fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                    Align(
-                      alignment: Alignment.centerRight,
-                      child: Container(
-                        padding: EdgeInsets.only(left: 5, right: 5, bottom: 3),
-                        margin: EdgeInsets.only(bottom: 15, top: 5),
-                        decoration: BoxDecoration(
-                            border: Border(
-                                bottom:
-                                    BorderSide(color: MyColors.myGreyDark))),
-                        child: Row(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Text(
-                              "location...",
-                              style: TextStyle(
-                                  fontSize: 16, fontWeight: FontWeight.bold),
+                  Row(
+                    children: [
+                      Container(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: const [
+                            Padding(
+                              padding: EdgeInsets.only(top: 20.0),
+                              child: Text(
+                                "Welcome User",
+                                style: TextStyle(
+                                    fontSize: 30,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black),
+                              ),
+
                             ),
+                            Padding(
+                              padding: EdgeInsets.only(left: 8.0),
+                              child: Text(
+                                "John samuel",
+                                style: TextStyle(
+                                    fontSize: 16, color:Colors.black54),
+                              ),
+                            ),
+
                             SizedBox(
-                              width: 5,
-                            ),
-                            Icon(
-                              Icons.location_on,
-                              size: 20,
+                              height: 5,
                             ),
                           ],
                         ),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 8.0, vertical: 15),
-                      child: SearchBox(onSubmit: (str) {}, hint: "Search..."),
-                    ),
-                    // Row(
-                    //   mainAxisSize: MainAxisSize.max,
-                    //   children: [
-                    //     Expanded(
-                    //       child: Divider(
-                    //         endIndent: 5,
-                    //         thickness: 2,
-                    //       ),
-                    //     ),
-                    //     Text(
-                    //       "OR",
-                    //       style: TextStyle(
-                    //           fontSize: 20,
-                    //           fontWeight: FontWeight.bold,
-                    //           color: MyColors.myGreyDark),
-                    //     ),
-                    //     Expanded(
-                    //       child: Divider(
-                    //         indent: 5,
-                    //         thickness: 2,
-                    //       ),
-                    //     )
-                    //   ],
-                    // ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Align(
-                          alignment: Alignment.centerLeft,
-                          child: Text(
-                            "Search Category",
-                            style: TextStyle(
-                                fontSize: 16, fontWeight: FontWeight.bold),
-                          )),
-                    ),
-                    Container(
-                      decoration: BoxDecoration(
-                          //color: Colors.white,
-                          borderRadius: BorderRadius.circular(20)),
-                      child: GridView(
-                        shrinkWrap: true,
-                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 2,
-                        ),
-                        children: [
-                          category(
-                              title: 'Mobiles and Computers',
-                              asset: 'assets/mobilephone.png'),
-                          category(
-                              title: 'Electronics and Appliances',
-                              asset: 'assets/electronics.png'),
-                          category(title: 'Hotels', asset: 'assets/hotel.png'),
-                          category(
-                              title: 'Travel Booking',
-                              asset: 'assets/travel.png'),
-                        ],
+                      Spacer(),
+                      Container(
+                        height: 50,
+                        width: 50,
+                        decoration: const BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(50)),
+                            color: MyColors.myGreyMid2,
+                            image: DecorationImage(
+                                image: AssetImage("assets/employe.png"))),
                       ),
-                    ),
-                  ],
-                ),
+                      SizedBox(height: 5,)
+                    ],
+                  ),
+                  Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      SizedBox(
+                        height: 12,
+                      ),
+
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 8.0, vertical: 15),
+                        child: SearchBox(onSubmit: (str) {}, hint: "Search..."),
+                      ),
+                      // Row(
+                      //   mainAxisSize: MainAxisSize.max,
+                      //   children: [
+                      //     Expanded(
+                      //       child: Divider(
+                      //         endIndent: 5,
+                      //         thickness: 2,
+                      //       ),
+                      //     ),
+                      //     Text(
+                      //       "OR",
+                      //       style: TextStyle(
+                      //           fontSize: 20,
+                      //           fontWeight: FontWeight.bold,
+                      //           color: MyColors.myGreyDark),
+                      //     ),
+                      //     Expanded(
+                      //       child: Divider(
+                      //         indent: 5,
+                      //         thickness: 2,
+                      //       ),
+                      //     )
+                      //   ],
+                      // ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text(
+                              "Search Category",
+                              style: TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.bold),
+                            )),
+                      ),
+                      Card(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20)
+                        ),color: Colors.white70,
+                        child: Container(
+                          decoration: BoxDecoration(
+                              //color: Colors.white,
+                              borderRadius: BorderRadius.circular(20)),
+                          child: GridView(
+                            shrinkWrap: true,
+                            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 2,
+                            ),
+                            children: [
+                              category(
+                                  title: 'Mobiles and Computers',
+                                  asset: 'assets/mobilephone.png'),
+                              category(
+                                  title: 'Electronics and Appliances',
+                                  asset: 'assets/electronics.png'),
+                              category(title: 'Hotels', asset: 'assets/hotel.png'),
+                              category(
+                                  title: 'Travel Booking',
+                                  asset: 'assets/travel.png'),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left:8.0,top:8,bottom:8),
+                        child: Align(alignment:Alignment.topLeft,
+                          child: Text("whats new", style: TextStyle(
+                              fontSize: 25, fontWeight: FontWeight.bold),),
+                        ),
+                      ),
+                      Card(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10)
+                        ),child: Container(
+                        height: 150,
+                        child: Row(
+                          children: [
+                            Expanded(
+                              flex: 1,
+                              child: Container(
+                                decoration: const BoxDecoration(
+                                  borderRadius:BorderRadius.all(Radius.circular(10),),
+                                  image:DecorationImage(
+                                    image: AssetImage('assets/poster.jpg'),
+                                    fit: BoxFit.fill,
+                                  ),color: MyColors.myGreyMid2,
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      ),
+                    ],
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
         ),
       ),
