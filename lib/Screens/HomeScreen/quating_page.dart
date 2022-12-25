99b1084aca772b516c463b73cfc7afec3282b3cf
mypc <spinegeneral@gmail.com>
2022-12-25 20:00:20 +0530

    pull

---
+++ lib/Screens/HomeScreen/quating_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class QuatingPage extends StatefulWidget {
+  const QuatingPage({Key? key}) : super(key: key);
+
+  @override
+  State<QuatingPage> createState() => _QuatingPageState();
+}
+
+class _QuatingPageState extends State<QuatingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child:Column(
+          children: [
+            Container(),
+            Container(),
+          ],
+        )
+      ),
+    );
+  }
+}
